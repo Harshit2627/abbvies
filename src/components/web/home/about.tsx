230d2e46d2ff9056bc62449863f56d8509156d1f
Ishika Rastogi <ishika.rastogi@innobitsystems.com>
2023-03-19 19:15:21 +0530

    vision and mission

---
+++ src/components/web/home/about.tsx
@@ -0,0 +1,48 @@
+import { Carousel, Col, Row } from "antd"
+import vision from "../../../assets/vision2.png"
+import { DotPosition } from "antd/es/carousel";
+import { useState } from "react";
+
+
+export const About = () => {
+    const [dotPosition, setDotPosition] = useState<DotPosition>('left');
+    return (
+        <>
+            <Row className="visionRow">
+                <Col xs={24} sm={24} md={8} lg={8} xl={8} xxl={8} className="vision body">
+                    <h1>Vision</h1>
+                    <p className="p visionPara body">
+                        Our vision states us to be a commercially viable leading
+                        pharmaceutical company in providing quality products affordable to
+                        all sections in the society. We aspire to be the best health care
+                        company which is dedicated to provide leading innovations and
+                        solution for tomorrow.
+                    </p>
+                </Col>
+                <Col xs={24} sm={24} md={8} lg={8} xl={8} xxl={8}>
+                    <Carousel dotPosition={dotPosition} effect="fade" autoplay autoplaySpeed={1000}>
+                        <div className="visionBannerdiv first-banner">
+
+                        </div>
+                        <div className="visionBannerdiv second-banner">
+
+                        </div>
+                        {/* <img src={vision} height="220" width="96%" className="visionBanner" />
+                        <img src={vision} height="220" width="96%" className="visionBanner" /> */}
+                    </Carousel>
+                </Col>
+                <Col xs={24} sm={24} md={8} lg={8} xl={8} xxl={8} className="vision body">
+                    <h1>Mission</h1>
+                    <p className="p visionPara body">
+                        We are responsible to the society, we shall be good, cooperate
+                        citizen and will be driven by with ethical standard in our
+                        practices. Abbvies Pharmaceuti- cal's prime mission is to protect
+                        con- sumers' health and wellbeing. We be- lieve that each patient
+                        consumes our medicines with trust and confidence.
+                    </p>
+                </Col>
+
+            </Row>
+        </>
+    )
+}
--- src/components/web/home/index.tsx
+++ src/components/web/home/index.tsx
@@ -1,4 +1,5 @@
 import { Carousel, Col, Row, Space } from "antd";
+import { About } from "./about";
 import "./homeStyles.css";
 
 export const Home = () => {
@@ -22,31 +23,7 @@ export const Home = () => {
           </Carousel>
         </Col>
       </Row>
-      <Row>
-        <Col span={24} style={{ padding: 10 }}>
-          <h1>About Us</h1>
-        </Col>
-        <Col xxl={12} xl={12} lg={12} md={24} style={{ padding: 10 }}>
-          <h1>Vision</h1>
-          <p>
-            Our vision states us to be a commercially viable leading
-            pharmaceutical company in providing quality products affordable to
-            all sections in the society. We aspire to be the best health care
-            company which is dedicated to provide leading innovations and
-            solution for tomorrow.
-          </p>
-        </Col>
-        <Col xxl={12} xl={12} lg={12} md={24} style={{ padding: 10 }}>
-          <h1>Mission</h1>
-          <p>
-            We are responsible to the society, we shall be good, cooperate
-            citizen and will be driven by with ethical standard in our
-            practices. Abbvies Pharmaceuti- cal's prime mission is to protect
-            con- sumers' health and wellbeing. We be- lieve that each patient
-            consumes our medicines with trust and confidence.
-          </p>
-        </Col>
-      </Row>
+      <About />
     </div>
   );
 };
